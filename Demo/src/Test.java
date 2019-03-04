6271352a907875ce41c7f0b0577f0721097d467b
WJJWYF <45487976+WJJWYF@users.noreply.github.com>
2019-03-04 21:46:54 +0800

    改良版接口.....(上一个不对）

--- Demo/src/Test.java
+++ Demo/src/Test.java
@@ -1,30 +1,34 @@
-interface Animal
+interface CanCry
 {
-	public abstract void CanCry();
+	public void cry();
 }
-class Dog implements Animal
+class Dog implements CanCry
 {
-	public void CanCry()
+	public void cry()
 	{
 		System.out.println("我是狗,我的叫声是汪汪汪");
 	}
 }
-class Cat implements Animal
+class Cat implements CanCry
 {
-	public void CanCry()
+	public void cry()
 	{
 		System.out.println("我是猫，我的叫声是汪汪汪");
 	}
 }
 class Test 
 {
+	void makeCry(CanCry c)
+	{
+		c.cry();
+	}
 	public static void main(String[] args)
 	{
-		Dog a=new Dog(); 
-		Cat b=new Cat();
-		Test c=new Test();
-		a.CanCry();
-		b.CanCry();
+		Dog dog=new Dog(); 
+		Cat cat=new Cat();
+		Test test=new Test();
+		test.makeCry(dog);
+		test.makeCry(cat);
 	}
 
 }
